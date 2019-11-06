9d82f7f59619f47401b461812a8d7d0b3fc61a3b
Hamdan Latief <44519762+azuluna@users.noreply.github.com>
2019-11-07 02:20:31 +0700

    update command function

---
+++ public/assets/js/node-1/notif.js
@@ -0,0 +1,82 @@
+var db = firebase.database()
+var GLOBAL_takeLastN = 0
+var GLOBAL_beginLimit = 0
+var GLOBAL_endLimit = 0
+
+async function main() {
+    let FETCH_STATUS = false
+    let FETCHED_DATA = []
+    let FILTERED_DATA = []
+
+    // // NGAMBIL SEMUA YG BELUM BELUM
+    // await db.ref('/vertical_garden').limitToLast(10).once('value', function (snapshot) {
+    //     FETCHED_DATA = []
+    //     snapshot.forEach(s => {
+    //         FETCHED_DATA.push(s.val())
+    //     })
+
+    //     if (GLOBAL_takeLastN == 0) {
+    //         GLOBAL_takeLastN = FETCHED_DATA.length
+    //     }
+
+    //     if (GLOBAL_beginLimit !== 0 && GLOBAL_endLimit !== 0) {
+    //         console.log('here');
+
+    //         FILTERED_DATA = FETCHED_DATA.slice(GLOBAL_beginLimit, GLOBAL_endLimit)
+    //     } else {
+    //         FILTERED_DATA = FETCHED_DATA.slice(Math.max(FETCHED_DATA.length - GLOBAL_takeLastN, 1))
+    //     }
+    // })
+
+    // NGAMBIL DATA BARU -> TAMPILIN
+    db.ref('/vertical_garden').endAt().limitToLast(1).on('child_added', function (snapshot) {
+        if (FETCH_STATUS) {
+            const {
+                parameter,
+                waktu
+            } = snapshot.val()
+
+            if (parameter.node == 'node-01') {
+                alert("wedhus!");
+                // return //bawahnya ga dieksekusi
+            }
+
+            // chart.data.labels.push(waktu.jam)
+            // chart.data.datasets[0].data.push(parameter.suhu)
+
+            // if (chart.data.datasets[0].data.length > GLOBAL_takeLastN) {
+            //     chart.data.labels.shift()
+            //     chart.data.datasets[0].data.shift()
+            // }
+
+            // chart.update();
+        }
+        FETCH_STATUS = true
+    })
+
+    // var ctx = document.getElementById('myChart-suhu').getContext('2d');
+    // var chart = new Chart(ctx, {
+    //     // The type of chart we want to create
+    //     type: 'line',
+
+    //     // The data for our dataset
+    //     data: {
+    //         labels: FILTERED_DATA.map(data => data.waktu.jam),
+    //         datasets: [{
+    //             label: "Temperature",
+    //             fill: false,
+    //             borderColor: 'rgb(0, 0, 153)',
+    //             data: FILTERED_DATA.map(data => data.parameter.suhu),
+    //         }]
+    //     },
+
+    //     // Configuration options go here
+    //     options: {
+    //         responsive: true,
+    //         maintainAspectRatio: false,
+    //     }
+    // });
+
+}
+
+main()
---
+++ public/assets/js/node-1/on-off.js
@@ -0,0 +1,45 @@
+        function funcoff() {
+            firebase.database().ref("pupuk").set({
+                servo_1: "off"
+            });
+            Swal.fire(
+                'Fertillizer OFF',
+                ' ',
+                'error'
+            );
+            getData();
+        }
+
+        function funcon() {
+            firebase.database().ref("pupuk").set({
+                servo_1: "on"
+            });
+            Swal.fire(
+                'Fertillizer ON',
+                ' ',
+                'success'
+            );
+            getData();
+        }
+
+        function funcoto() {
+            firebase.database().ref("pupuk").set({
+                servo_1: "on"
+            });
+            Swal.fire(
+                'Fertillizer ON',
+                'Otomatically off in 10 seconds',
+                'warning'
+            );
+            getData();
+        }
+
+        function getData() {
+            firebase.database().ref('/').once('value', function (snapshot) {
+                snapshot.forEach(function (childSnapshot) {
+                    var childData = childSnapshot.val();
+                    document.getElementById("data").innerHTML = "Pupuk: " + childData['servo_1'];
+                })
+            })
+        }
+        getData();
